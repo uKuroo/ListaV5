6597384fec90652017a4a634081d00a82e6eb2c5
Gabriel Nacimento <opitimalracer50@hotmail.com>
2024-04-20 01:17:52 -0300

    Add quest2

---
+++ Quest2/Q2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int base, area, perimetro;
+    
+    printf("Insira o lado do quadrado\n");
+    scanf("%d",&base);
+    
+    area = base * base;
+    perimetro = base * 4;
+    
+    printf("Área do quadrado: %d\n",area);
+    printf("Perímetro do quadrado: %d",perimetro);
+    
+
+    return 0;
+}
+
